e5157097b05c46194119b1f16e5dc2a32d531cfa
Nathan Palmer <nathan@nathanpalmer.com>
2015-02-03 15:18:31 -0500

    Added the ajax util

---
+++ app/utils/ajax.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default function ajax(hash) {
+  return new Ember.RSVP.Promise(function(resolve, reject) {
+    hash.success = function(json) {
+      Ember.run(null, resolve, json);
+    };
+
+    hash.error = function(jqXHR, textStatus, errorThrown) {
+      Ember.run(null, reject, jqXHR);
+    };
+
+    Ember.$.ajax(hash);
+  });
+}
---
+++ tests/unit/utils/ajax-test.js
@@ -0,0 +1,9 @@
+import ajax from 'uncharted-ajax/utils/ajax';
+
+module('ajax');
+
+// Replace this with your real tests.
+test('it works', function() {
+  var result = ajax();
+  ok(result);
+});
