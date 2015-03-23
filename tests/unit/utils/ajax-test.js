7bc5841c3fef374af76002fb2abc625b1c8be07d
Nathan Palmer <nathan@nathanpalmer.com>
2015-03-23 09:54:02 -0400

    Added a couple of tests for the multiple formats

--- tests/unit/utils/ajax-test.js
+++ tests/unit/utils/ajax-test.js
@@ -1,9 +1,37 @@
 import ajax from 'uncharted-ajax/utils/ajax';
+import Pretender from 'pretender';
 
-module('ajax');
+var pretender = null;
+
+module('ajax', {
+  setup: function() {
+    pretender = new Pretender();
+  },
+
+  teardown: function() {
+    pretender.shutdown();
+  }
+});
 
 // Replace this with your real tests.
-test('it works', function() {
-  var result = ajax();
+test('it works with a hash', function() {
+  expect(2);
+
+  pretender.get('/url', function(request) {
+    ok(request);
+  });
+
+  var result = ajax({ url: '/url' });
+  ok(result);
+});
+
+test('it works with a string', function() {
+  expect(2);
+
+  pretender.get('/url', function(request) {
+    ok(request);
+  });
+
+  var result = ajax('/url');
   ok(result);
 });
